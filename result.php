7c719905f8a273358f9e6d67cd6a2345e0250dc8
Naguib <moma.naguib@gmail.com>
2018-02-15 12:00:00 +0100

    Ajout du fichier result.php pour afficher le message envoyé

---
+++ result.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Result</title>
+</head>
+<body>
+    <h2>Message Sent!</h2>
+    <p><strong>Name:</strong> <?php echo $_GET['name']; ?></p>
+    <p><strong>Email:</strong> <?php echo $_GET['email']; ?></p>
+    <p><strong>Message:</strong> <?php echo $_GET['message']; ?></p>
+</body>
+</html>
